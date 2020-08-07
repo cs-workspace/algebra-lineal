585d46be45289ccbe9b03f197398dbdca6a15f85
steven-n-wilson <49218333+steven-n-wilson@users.noreply.github.com>
2020-08-07 15:57:16 -0600

    finished excercises 4 5

--- R/LAB1_introR.r
+++ R/LAB1_introR.r
@@ -12,11 +12,9 @@ sprintf('Num1 =  %i y Num2 %i', Num1, Num2)
 # Ejercicio 2 
 
 Resultados = c(Num1 + Num2, Num1 - Num2, Num1 * Num2, Num1 / Num2, Num1 ** Num2 )
-print()
 print(Resultados)
 
 # Ejercicio 3
-print()
 sprintf('La suma entre %i y Num2 %i es %i', Num1, Num2, Resultados[1])
 sprintf('La resta entre %i y Num2 %i es %i', Num1, Num2, Resultados[2])
 sprintf('La multiplicacion entre %i y Num2 %i es %i', Num1, Num2, Resultados[3])
@@ -33,3 +31,25 @@ matriz = rbind(
     c(1,2,3,4,5,6,7), 
     c(1,2,3,4,5,6,7),
     c(1,2,3,4,5,6,7))
+
+count = 1  
+for(row in 1:nrow(matriz)) {   
+    print(matriz[count, ])
+    count = count + 1
+    
+}
+
+# Ejercicio 5
+count = 1
+for(row in 1:nrow(matriz)) {   
+    matriz[count, count] = 99
+    count = count + 1
+    
+}
+
+count = 1
+for(row in 1:nrow(matriz)) {   
+    print(matriz[count, ])
+    count = count + 1
+    
+}
