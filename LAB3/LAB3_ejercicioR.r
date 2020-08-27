23f6bb03ffb1b543ab88250d0ed2d32353c39a49
steven-n-wilson <49218333+steven-n-wilson@users.noreply.github.com>
2020-08-27 15:45:58 -0600

    added excercises

---
+++ LAB3/LAB3_ejercicioR.r
@@ -0,0 +1,44 @@
+# Algebra Lineal
+# Entrega: 31/8/2020
+# @steven_wilson
+
+
+library('pracma')
+
+
+# 1. Matrices A y B tamano 15 x 15 numeros aleatorios entre (-10, 10)
+A <- matrix(data = sample(-10:10, 225, TRUE), nrow = 15, ncol = 15)
+print('Matriz A:')
+print(A)
+
+B <- matrix(data = sample(-10:10, 225, TRUE), nrow = 15, ncol = 15)
+print('Matriz B:')
+print(B)
+
+
+# 2. Suma de matrices del mismo tamano
+print ('Suma de matrices A + B') 
+print(A + B)
+
+
+# 3. Diferencia de matrices del mismo tamano
+print ('Diferemcia de matrices A - B') 
+print(A - B)
+
+
+# 4. Producto componente por componente entre A y B.
+
+
+# 5. Producto Matricial BA
+print('Producto matricial BA' )
+print(B %*% A) 
+
+
+# 6. FERR A
+print('FERR A')
+rref(A)
+
+
+# 7. FERR B
+print('FERR B')
+rref(B)
