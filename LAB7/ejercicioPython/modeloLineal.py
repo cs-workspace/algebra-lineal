0520d0dad9da5093fc1f355d1bd0f6fd349ab88a
steven-n-wilson <49218333+steven-n-wilson@users.noreply.github.com>
2020-10-06 11:57:07 -0600

    adds excercise models in python

--- LAB7/ejercicioPython/modeloLineal.py
+++ LAB7/ejercicioPython/modeloLineal.py
@@ -10,29 +10,26 @@ import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
 
-# Coronavirus - Cases vs Days
-df = pd.read_excel("ejercicio/Data.xlsx", sheet_name='Mexponencial')
+# GRE - Admission
+df = pd.read_excel("ejercicio/Data.xlsx", sheet_name='Mlineal')
 
-x = ((df.iloc[:, 1]).to_numpy())
-x = x-x[0]
-y = ((df.iloc[:, 2]).to_numpy())
+x = ((df.iloc[:, 0]).to_numpy())
+y = ((df.iloc[:, 1]).to_numpy())
 
-p = np.polyfit(x, np.log(y), 1)
+p = np.polyfit(x, y, 1)
 
-Po = np.exp(p[1])
-r = p[0]
+print('GRE - Admission\n', p)
 
-print('Coronavirus - Cases vs Days\n', r, Po)
 
 # Valores de y calculados del ajuste
-y_ajuste = Po * np.exp(r*x)
+y_ajuste = p[0]*x + p[1]
 
 # Dibujamos los datos experimentales
 p_datos, = plt.plot(x, y, 'b.')
 # Dibujamos la recta de ajuste
 p_ajuste, = plt.plot(x, y_ajuste, 'r-')
 
-plt.title('Modelo Exponencial')
+plt.title('Recta de mejor ajuste')
 
 plt.xlabel('Eje X')
 plt.ylabel('Eje Y')
